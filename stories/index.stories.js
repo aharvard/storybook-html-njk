d1f5daa540c7718c70395af8bc6e88537d531e6c
Andrew Harvard <andrew.t.harvard@gmail.com>
2019-08-25 14:30:48 -0500

    inital commit

---
+++ stories/index.stories.js
@@ -0,0 +1,9 @@
+import { storiesOf } from "@storybook/html";
+
+import index from "../src/index.njk";
+import component from "../src/components.njk";
+
+storiesOf("Demo", module)
+  .add("heading", () => "<h1>Hello World</h1>")
+  .add("nunjucks index", () => index)
+  .add("nunjucks button", () => component.button);
