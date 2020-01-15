c34777263d498c51d6c738f9fd50e1ca3f923bae
Alexey <lesha.ogonkov@gmail.com>
2020-01-15 16:15:43 +0300

    Fix templates render

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,9 +2,9 @@ import { storiesOf } from "@storybook/html";
 
 storiesOf("Demo", module)
   .add("heading", () => "<h1>Hello World</h1>")
-  .add("nunjucks index", () => {
+  .add("nunjucks index", () => (
     require("../src/index.njk").render({});
-  })
-  .add("nunjucks button", () => {
+  ))
+  .add("nunjucks button", () => (
     require("../src/components.njk").render({});
-  });
+  ));
